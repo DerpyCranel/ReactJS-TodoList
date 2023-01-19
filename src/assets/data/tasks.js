fcba2ae34ac94dd4a4a0b2f2b9d9c21e211a44d0
Derpy <miguel45klm@gmail.com>
2023-01-18 19:42:35 -0600

    version 1.0

---
+++ src/assets/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks=[
+    {
+        id:0,
+        title:'primera tarea',
+        description:'crear aplicación en reactjs',
+    },
+    {
+        id:1,
+        title:'segunda tarea',
+        description:'subir aplicación a producción',
+    },
+    {
+        id:2,
+        title:'tercera tarea',
+        description:'compartir aplicación',
+    }
+];
